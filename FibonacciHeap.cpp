48dad2eba15f22516ef7ba4bf120191d47a55ac5
Maxim Busel <sajeruk@gmail.com>
2013-09-29 22:29:38 +0300

    Initial Commit
    Contains nothing, except base files of future Fibonacci heap

---
+++ FibonacciHeap.cpp
@@ -0,0 +1,17 @@
+#ifndef FIBONACCI_HEAP_CPP
+#define FIBONACCI_HEAP_CPP
+#include "FibonacciHeap.h"
+
+template <typename T>
+FibonacciHeap<T>::FibonacciHeap(void) {
+}
+
+template <typename T>
+FibonacciHeap<T>::~FibonacciHeap(void) {
+}
+
+template <typename T>
+void FibonacciHeap<T>::insert(T item) {
+
+}
+#endif
---
+++ FibonacciHeap.h
@@ -0,0 +1,27 @@
+//created by Maxim Busel @sajeruk
+#ifndef FIBONACCI_HEAP_H
+#define FIBONACCI_HEAP_H
+
+/*
+
+*/
+template <typename T>
+class FibonacciHeap
+{
+public:
+  inline FibonacciHeap(void);
+  inline virtual ~FibonacciHeap(void);
+
+  void insert(T item);
+  /*T findMin() {};
+  void removeMin() {};
+  void remove(T item) {};
+  void modify(T item, T modification) {};
+  FibonacciHeap<T> merge(FibonacciHeap<T> fh) {};
+  FibonacciHeap<T> operator + (FibonacciHeap fh) {};*/
+
+};
+
+
+#endif
+
